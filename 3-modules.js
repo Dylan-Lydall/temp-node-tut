b2d5e8616d96f683771f24496496c021a206eb46
Dylan Lydall <dylanlydall@gmail.com>
2024-07-15 10:38:45 +0200

    Initial commit

---
+++ 3-modules.js
@@ -0,0 +1,8 @@
+const { john, peter } = require("./4-names");
+const sayHi = require("./5-utils");
+const { items, person } = require("./6-alternative-flavour");
+
+require("./7-mind-grenade");
+sayHi("susan");
+sayHi(john);
+sayHi(peter);
